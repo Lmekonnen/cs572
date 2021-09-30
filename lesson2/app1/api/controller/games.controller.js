67c0c43c77c3c1fd119ea7f6af595c4f9ececab8
Lmekonnen <89761413+Lmekonnen@users.noreply.github.com>
2021-09-30 18:58:23 -0500

    update

--- lesson2/app1/api/controller/games.controller.js
+++ lesson2/app1/api/controller/games.controller.js
@@ -4,6 +4,7 @@ const dbconnection = require("../data/dbconnection");
 module.exports.gamesGetAll=function(req,res){
     console.log("Json got request")
     const dbconnection=dbconnection.get()
+    const gamesCollection=db.collection("games")
     console.log("db is",db)
     let Offset=0
     let count=5
@@ -16,10 +17,14 @@ if(req.query && req.query.count)
 {
     count=parseInt(req.query.count)
 }    
-console.log("offset",Offset)
-console.log("offset+count",Offset+count)
-const pageGames=gamesData.slice(Offset,Offset+count)
-    res.status(200).json(gamesData);};
+// console.log("offset",Offset)
+// console.log("offset+count",Offset+count)
+// const pageGames=gamesData.slice(Offset,Offset+count)
+gamesCollection.find().skip(Offset).limit(count).toArray(function(err,games){
+    console.log("found games",games)
+    res.status(200).json(games)
+})
+    // res.status(200).json(gamesData);};
     gamesAddone=function(req,res){
         console.log("Json got request")
-        res.status(200).json({"json":true});};
+        res.status(200).json({"json":true})}}
---
+++ lesson2/app2/api/controller/games.controller.js
@@ -0,0 +1,25 @@
+const gamesData=require("../data/games.json")
+const dbconnection=require("../data/dbconnection");
+const dbconnection = require("../data/dbconnection");
+module.exports.gamesGetAll=function(req,res){
+    console.log("Json got request")
+    const dbconnection=dbconnection.get()
+    console.log("db is",db)
+    let Offset=0
+    let count=5
+    console.log("req.query",req.query )
+    if(req.query && req.query.Offset)
+{
+    Offset=parseInt(req.query.Offset)
+}   
+if(req.query && req.query.count)
+{
+    count=parseInt(req.query.count)
+}    
+console.log("offset",Offset)
+console.log("offset+count",Offset+count)
+const pageGames=gamesData.slice(Offset,Offset+count)
+    res.status(200).json(gamesData);};
+    gamesAddone=function(req,res){
+        console.log("Json got request")
+        res.status(200).json({"json":true});};
